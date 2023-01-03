7a435d7c33528ad46372890110d3300280b33c29
jchinedu <jchinedu080@gmail.com>
2023-01-03 08:03:35 -0800

    git

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -4,4 +4,5 @@
 
 """Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
-   print("{}".format(chr(letter)), end="")
+    :wq
+    print("{}".format(chr(letter)), end="")
